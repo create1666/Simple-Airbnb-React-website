46fe6ce39abf688e8aefecba7b751d1c983d3b4a
create1666 <create1666@gmail.com>
2021-06-30 00:12:14 +0100

    fix:

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const cardObj1 = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Time nah money... ",
+  headLine: "Run life-time tranzation wey no de give headache",
+  description: "way wey we no de joke with our exclusive ogbonge card we de give nna life-time tranzation wey no require charges fee",
+  buttonLabel: "Get started",
+  imgStart: "",
+  img: 'images/creditCard-3.svg',
+  alt: 'Credit Card',
+  title: ''
+}
+
+export const cardObj2 = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Time nah money... ",
+  headLine: "Run life-time tranzation wey no de give headache",
+  description: "way wey we no de joke with our exclusive ogbonge card we de give nna life-time tranzation wey no require charges fee",
+  buttonLabel: "Get started",
+  imgStart: "",
+  img: 'images/creditCard-3.svg',
+  alt: 'Credit Card',
+  title: ''
+}
+
+
+export const cardObj3 = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Time nah money... ",
+  headLine: "Run life-time tranzation wey no de give headache",
+  description: "way wey we no de joke with our exclusive ogbonge card we de give nna life-time tranzation wey no require charges fee",
+  buttonLabel: "Get started",
+  imgStart: "",
+  img: 'images/creditCard-3.svg',
+  alt: 'Credit Card',
+  title: ''
+}
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Banner from './Banner'
+import Card from './Card'
 import './Home.css'
+import {cardObj1, cardObj2, cardObj3} from './Data'
 
 const Home = () => {
+  const {img, src, tittle} = cardObj1
+
   return (
     <div className='home'>
-      
       <Banner/>
+      <div>
+        <Card img ={img} src ={src} title={tittle}/>
+        <Card {...cardObj2}/>
+        <Card {...cardObj3}/>
+      </div>
+
+      <div>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+
     </div>
   )
 }
